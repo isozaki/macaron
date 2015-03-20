228eafc6ebb7d919d7648391bc6c9f72ae24cdb3
t-shimada <shimada@wacom-it.co.jp>
2015-03-20 16:03:18 +0900

    回答内容を改行して表示する対応。質問一覧画面のrspecを修正

--- q_and_a/spec/views/questions/show.html.erb_spec.rb
+++ q_and_a/spec/views/questions/show.html.erb_spec.rb
@@ -2,10 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "questions/show.html.erb", :type => :view do
   before(:each) do
-    @question = mock_model(Question)
-    allow(@question.limit_datetime).to receive(:strftime)
-    allow(@question.created_at).to receive(:in_time_zone)
-    allow(@question.created_at).to receive(:strftime)
+    @question = FactoryGirl.create(:question)
     @answers = mock_model(Answer)
     expect(@answers).to receive(:each).and_return(@answers)
     assign(:question, @question)
