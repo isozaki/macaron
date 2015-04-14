82929c76ee08ab07bb2032cad673cdb91946ddee
t-shimada <shimada@wacom-it.co.jp>
2015-04-14 17:35:37 +0900

    ログイン機能を実装(ログインをチェックするフィルターは未実装)

---
+++ q_and_a/app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+    user = User.find_by_login(params[:login])
+    if user && user.authenticate(params[:pass]) 
+      session[:user_id] = user.id
+      redirect_to(menus_path, notice: 'ログインしました')
+    else
+      redirect_to(new_session_path, alert: 'ログインに失敗しました')
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    @logind_user = nil
+    redirect_to(new_session_path, notice: 'ログアウトしました')
+  end
+end
--- q_and_a/config/routes.rb
+++ q_and_a/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :session, except: [:show, :update]
+
   resources :menus, only: [:index]
 
   resources :questions do
