7755e0aaaad753d3271f93d015d1395ac621263f
t-shimada <shimada@wacom-it.co.jp>
2015-05-21 17:16:52 +0900

    questionモデルにmatter_idを追加

--- q_and_a/app/models/matter.rb
+++ q_and_a/app/models/matter.rb
@@ -13,6 +13,7 @@ class Matter < ActiveRecord::Base
   validates(:title, presence: true, length: { maximum: 255 })
   
   has_many(:matter_users)
+  has_many(:questions)
 
   def self.search_matter(params)
     matters = Matter.order('id ASC')
